6c5a0b8e3c8fa1ad81f3d3bf9d66ad043fd8cf31
eyneill777 <eyneill777@gmail.com>
2023-03-07 22:55:03 -0500

    Replaced the api endpoint for uploading files
     - Removed base64 encoding from file upload
     - Upload now filters for configured file extension.
     - Added placeholder headers for username and api key
     - File now saves with a generated uuid for the filename
     - Added appropriate error responses to the file upload

--- API/APITester.py
+++ API/APITester.py
@@ -1,14 +1,14 @@
 import requests
-import base64
+
 
 
 
 #Upload an image using the API
-data = open("/Users/eron/Downloads/im.jpeg", "rb")
-data_read = data.read()
-data_64_encode = base64.b64encode(data_read) 
-data_64_string = data_64_encode.decode('utf-8')
-payload = {'id': 'test', 'file': {'base64': data_64_string, 'extension': '.jpeg'}}
-response = requests.post("http://localhost:5000/upload/", json = payload)
+file = open("/Users/eron/Downloads/im.jpeg", "rb")
+
+files = {"file": file}
+headers = {'id': 'test', "userName": "test user", "apiKey": "1234"}
+
+response = requests.post("http://localhost:5000/upload/", files=files, headers=headers)
 print(response)
 print(response.text)
