c70d3b75b7aff96d7685e764862948b02a1e1ecd
leahschneidereit <118493180+leahschneidereit@users.noreply.github.com>
2023-03-20 18:01:30 -0400

    update

--- App Front End/connection.py
+++ App Front End/connection.py
@@ -1,9 +1,14 @@
 import mysql.connector
 from mysql.connector import Error
+import json
 
 def dbconnection():
+
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
     try:
-        conn = mysql.connector.connect(host='localhost', database='seeshell',user='root',password='')
+        conn = mysql.connector.connect(host=config['host'], database=config['database'], user=config['username'],password=config['password'])
 
     except Error as e:
         print('Connection Error')
