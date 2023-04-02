f974f4654347cac7e4d85a94eaa01cb3da58ff32
michaelaann907 <michaelaap907@gmail.com>
2023-04-02 02:40:32 -0400

    Updated Layout updated Add button works and uploads images Delete Button (currently does not work) Camera Button (currently does not work) Image selection (in progress)

--- App Front End/Gallery.py
+++ App Front End/Gallery.py
@@ -1,68 +1,113 @@
-import kivy
+
+import os
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
-from kivy.core.window import Window
-from plyer import filechooser
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty, BooleanProperty
 
 
-class PhotoAlbum(App):
-    def build(self):
-        # create the main layout
-        main_layout = BoxLayout(orientation='vertical')
+class Gallery(GridLayout):
+    def __init__(self, **kwargs):
+        super(Gallery, self).__init__(**kwargs)
+        self.cols = 3  # Set the number of columns in the grid
+        self.spacing = 5  # Set the spacing between widgets in the grid
 
-        # create a list to store the images
-        self.images = []
+        # Add images to the grid
+        for i in range(1, 10):
+            image = Image(source=f"image{i}.jpg")
+            self.add_widget(image)
 
-        # create a button to add images
-        self.add_btn = Button(text='Add Image', size_hint=(None, None), size=(100, 50), pos=(650, 40))
-        self.delete_btn = Button(text='Delete Image', size_hint=(None, None), size=(100, 50), pos=(535, 40))
-
-        self.add_btn.bind(on_press=self.open_file_chooser)
-        self.delete_btn.bind(on_press=self.delete_widget)
-
-
-        # add the button to the main layout
-        main_layout.add_widget(self.add_btn)
-        main_layout.add_widget(self.delete_btn)
-
-        #make images selectable
+        # Add buttons to the bottom of the screen
+        button1 = Button(text="Add Image")
+        self.add_widget(button1)
+        button2 = Button(text="Camera")
+        self.add_widget(button2)
+        button3 = Button(text="Delete Image")
+        self.add_widget(button3)
 
 
-        return main_layout
-
-    def delete_widget(self):
-        # remove the image from the gallery
-        self.remove_widget(self.delete_btn)
 
 
-    def open_file_chooser(self, instance):
-        # open the file chooser
-        filechooser.open_file(on_selection=self.load_image)
 
-    def load_image(self, selection):
-        # check if a file was selected
+
+
+class SelectableImage(Image):
+    selected = BooleanProperty(False)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self.selected = not self.selected
+            return True
+        return super(SelectableImage, self).on_touch_down(touch)
+
+class PhotoAlbum(GridLayout):
+    images = ListProperty([])
+
+    def __init__(self, **kwargs):
+        super(PhotoAlbum, self).__init__(**kwargs)
+        self.cols = 3
+        self.spacing = 10
+
+        # create the 'Add Image' button
+        #self.add_widget(Button(text='Add Image', on_press=self.add_image))
+
+        self.upload_button = Button(text='Add Image', size_hint=(None,None), size=(200,50))
+        self.upload_button.bind(on_press=self.add_image)
+
+        self.delete_button = Button(text='Delete', size_hint=(None, None), size=(200, 50))
+        self.delete_button.bind()
+
+        self.camera_btn = Button(text='Camera', size_hint=(None,None), size=(200,50))
+        self.camera_btn.bind()
+
+        self.add_widget(self.upload_button)
+        self.add_widget(self.delete_button)
+        self.add_widget(self.camera_btn)
+        # Add buttons to the bottom of the screen
+
+
+    def add_image(self, *args):
+        # open a file selection dialog and get the selected file
+        from kivy.uix.filechooser import FileChooserIconView
+        chooser = FileChooserIconView()
+        chooser.path = os.path.expanduser('~')
+        chooser.filters = ['*.png', '*.jpg', '*.jpeg']
+        chooser.bind(selection=self.load_image)
+        popup = BoxLayout()
+        popup.add_widget(chooser)
+        App.get_running_app().root_window.add_widget(popup)
+
+    def load_image(self, chooser, selection):
         if selection:
-            # create the image widget and add it to the main layout
-            image = Image(source=selection[0], size_hint=(1, None))
-            self.images.append(image)
-            self.root.add_widget(image)
-        else:
-            # create a popup to inform the user that no file was selected
-            popup = Popup(title='Error',
-                          content=Label(text='No file selected.'),
-                          size_hint=(None, None),
-                          size=(Window.width * 0.8, Window.height * 0.2))
-            popup.open()
+            # create a new selectable image widget and add it to the album
+            img = SelectableImage(source=selection[0], allow_stretch=True)
+            img.delete_button = Button(text='Delete', size_hint_y=None, height=40)
+            img.delete_button.bind(on_press=lambda x: self.remove_image(img))
+            self.images.append(img)
+            self.add_widget(img)
+        chooser.parent.parent.remove_widget(chooser.parent)
+
+    class SelectableImage(ToggleButtonBehavior, Image):
+        selected = BooleanProperty(False)
+
+        def __init__(self, **kwargs):
+            super(SelectableImage, self).__init__(**kwargs)
+
+        def on_state(self, widget, value):
+            self.selected = value == 'down'
+
+        def remove_image(self, img):
+            # remove the selected image from the album
+            self.images.remove(img)
+            self.remove_widget(img)
 
 
-
-
-
+class GalleryApp(App):
+    def build(self):
+        return PhotoAlbum()
 
 if __name__ == '__main__':
-    PhotoAlbum().run()
+    GalleryApp().run()
