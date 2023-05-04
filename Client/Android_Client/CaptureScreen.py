a35400657933fc1b3994faed9a51e1964cd36126
ThomasSmith11 <wts205393@gmail.com>
2023-05-03 20:54:16 -0400

    captured images converted to png, directory generated by capturing images now removed

--- Client/Android_Client/CaptureScreen.py
+++ Client/Android_Client/CaptureScreen.py
@@ -65,15 +65,20 @@ class captureScreen(SeeShellScreen):
         '''
         Moves captured photo into the Photos folder from the automatically generated date folder upon image capture
         '''
-        filename = file_path.split('/')[-1]
+        im = PIL.Image.open(file_path)
+        im = im.convert('RGBA')
+        new_file_path = file_path.split('.')[0]+'.png'
+        im.save(new_file_path, format='PNG')
+        filename = new_file_path.split('/')[-1]
         img_id = str(uuid.uuid4())
         with open(file_path, 'rb') as f:
             self.api.uploadImage(img_id, f)
             f.close()
         self.call_message_service(img_id)
-        shutil.move(file_path, 'Photos')
-        filetype = filename.split('.')[1]
-        newname = "{}.{}".format(img_id, filetype)
+        shutil.move(new_file_path, 'Photos')
+        os.remove(file_path)
+        shutil.rmtree(file_path.split('/')[-2])
+        newname = "{}.png".format(img_id)
         newpath = os.path.join('Photos', newname)
         oldpath = os.path.join('Photos', filename)
         os.rename(oldpath, newpath)
